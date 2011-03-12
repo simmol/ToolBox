9409f66c92f2f1170981f5687300039f43090ab1
Anna Milenkova <anna.milenkova@gmail.com>
2011-03-12 18:39:29 +0200

    crawler final version; added 2 control parameters: -u(--url) -> url to crawl in; -o(--output) -> name of file to write in (in outout directory)

--- crawling.py
+++ crawling.py
@@ -4,37 +4,81 @@ import mechanize
 import re
 from mechanize import Browser,LinkNotFoundError
 import codecs
+from operator import itemgetter
+import getopt
+import sys
 
+maxPages = 15 # maximum pages to crawl in
+perPage = 33 # topics per page - used to determine next page url
 
+# name of output file (out by default)
+out = "out"
 
-f=codecs.open('out.txt',mode = 'w', encoding = 'utf-8')
+# reads link and output file name for forum from input
+optlist, args = getopt.getopt(sys.argv[1:], 'u:o:', ["url=", "output="])
 
+for optpair in optlist:
+    opt, arg = optpair
+    if opt in ("-u", "--url"):
+      url = arg
+    elif opt in ("-o", "--output"):
+      out = arg
+
+# open output file in utf-8 encoding
+f=codecs.open('output/'+out+'.txt' ,mode = 'w', encoding = 'utf-8')
+# initializing list of data
+data = [0]
+
+# make instance of browser 
 br = mechanize.Browser()
-br.open("http://rockplace.forumotion.net/f18-forum")
+br.open(url)
 
-nextPage = 0
-while nextPage == 0: #pages in forum
+# current page
+page = 1
+while page>0:
   url = br.geturl()
   nextElement = 0
-
-  while nextElement >= 0:  #topics on page
+  
+  # topics on page
+  while nextElement >= 0:  
     try:
       br.follow_link(url_regex=r"t[0-9]*\-topic$", nr=nextElement)
       nextElement += 1
 
-      print unicode(br.title(), 'cp1251', 'ignore')
-      print >>f, unicode(br.title(), 'cp1251', 'ignore'), br.geturl()
-      #f.write (unicode(br.title(), 'cp1251', 'ignore'))
+      # extract title and url (add in data)
+      print 'Crawling: '+ unicode(br.title(), 'cp1251', 'ignore')
+      data.append( ( unicode(br.title(), 'cp1251', 'ignore'), br.geturl() ) )
       
-      br.open(url) # go back to home page
+      
+      # go back to home page
+      br.open(url) 
+
+    #last topic
     except LinkNotFoundError:
-      nextElement = -1 #last topic
+      nextElement = -1 
 
   try:
     br.open(url)
-    br.follow_link(url_regex=r"p[0-9]*\-forum$", nr=0)
-    print "Next Page"
+    regEx = r"p"+ str(perPage*page) +"\-forum$"
+    if page != maxPages:
+      br.follow_link(url_regex=regEx, nr=0)
+      print "Next Page"
+      page += 1
+
+  #last page
   except LinkNotFoundError:
-    nextPage=-1
+    page=-1
+
+# remove the element for init; turn to set to eliminate duplicates; sort
+data.remove(0)
+data = set(data)
+data = sorted(data, key = itemgetter(0))
+
+# printing in output file in form for publishing in the forum
+for i in data:
+  topic, link = i
+  print "Adding: "+topic + " " + link
+  print >>f, '[url='+link+']'+topic+'[/url]'
+
 
 f.close()
