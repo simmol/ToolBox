d2b247a1a78e6d98ccfec730e4248c1db79dd18d
Anna Milenkova <anna.milenkova@gmail.com>
2011-03-12 20:12:36 +0200

    Last version of crawler: added -s parameter for silent execution

--- crawling.py
+++ crawling.py
@@ -8,14 +8,14 @@ from operator import itemgetter
 import getopt
 import sys
 
-maxPages = 15 # maximum pages to crawl in
+maxPages = 99 # maximum pages to crawl in
 perPage = 33 # topics per page - used to determine next page url
+silent = False # if silent is true - don't print.
+out = "out" # name of output file (out by default)
 
-# name of output file (out by default)
-out = "out"
 
 # reads link and output file name for forum from input
-optlist, args = getopt.getopt(sys.argv[1:], 'u:o:', ["url=", "output="])
+optlist, args = getopt.getopt(sys.argv[1:], 'su:o:', ["url=", "output="])
 
 for optpair in optlist:
     opt, arg = optpair
@@ -23,6 +23,8 @@ for optpair in optlist:
       url = arg
     elif opt in ("-o", "--output"):
       out = arg
+    elif opt == "-s":
+      silent = True
 
 # open output file in utf-8 encoding
 f=codecs.open('output/'+out+'.txt' ,mode = 'w', encoding = 'utf-8')
@@ -46,7 +48,8 @@ while page>0:
       nextElement += 1
 
       # extract title and url (add in data)
-      print 'Crawling: '+ unicode(br.title(), 'cp1251', 'ignore')
+      if not silent:
+        print 'Crawling: '+ unicode(br.title(), 'cp1251', 'ignore')
       data.append( ( unicode(br.title(), 'cp1251', 'ignore'), br.geturl() ) )
       
       
@@ -62,7 +65,8 @@ while page>0:
     regEx = r"p"+ str(perPage*page) +"\-forum$"
     if page != maxPages:
       br.follow_link(url_regex=regEx, nr=0)
-      print "Next Page"
+      if not silent:
+        print "Next Page"
       page += 1
 
   #last page
@@ -77,8 +81,10 @@ data = sorted(data, key = itemgetter(0))
 # printing in output file in form for publishing in the forum
 for i in data:
   topic, link = i
-  print "Adding: "+topic + " " + link
+  if not silent:
+    print "Adding: "+topic + " " + link
   print >>f, '[url='+link+']'+topic+'[/url]'
 
-
 f.close()
+
+sys.exit(0)
